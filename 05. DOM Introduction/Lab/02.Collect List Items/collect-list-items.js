5743888275a7975bc528ecee6364fa8667e54e0e
bojidarapartenova <bpartenova@gmail.com>
2025-11-18 00:27:54 +0200

    Add Lab

--- 05. DOM Introduction/Lab/02.Collect List Items/collect-list-items.js
+++ 05. DOM Introduction/Lab/02.Collect List Items/collect-list-items.js
@@ -1,3 +1,10 @@
 function extractText() {
-    // TODO
+    let listElements = document.querySelectorAll('li');
+    let textArea = document.querySelector('#result');
+
+    for (let listElement of listElements) {
+        let text = listElement.textContent;
+
+        textArea.textContent += text + '\n';
+    }
 }
--- 05. DOM Introduction/Lab/04.Show More/show-more.js
+++ 05. DOM Introduction/Lab/04.Show More/show-more.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+    let textToHide = document.getElementById('more');
+    let textToShow = document.getElementById('text');
+
+    textToHide.style.display = 'none';
+    textToShow.style.display = 'inline';
 }
